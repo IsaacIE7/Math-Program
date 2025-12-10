794d91cfa15080a68444eec39893dd2e7934dfc9
Isaac Elgazali <isaacelgazali@gmail.com>
2025-12-10 14:11:21 +0000

    Added lambda expressions in derivative and started integral methods.

--- Calculus.java
+++ Calculus.java
@@ -1,11 +1,22 @@
-public class Calculus {
+public class Calculus implements Function1D{
+    private static final double dx = 1e-7;
+    private static final double dt = 1e-4;
+    private static final double large = Math.pow(10, 15);
 
     public static Function1D derivative(Function1D f){
-        double h = 0.000000000000000001;
-        return null;
+        return x -> (f.apply(x + dx) - f.apply(x - dx))/(2 * dx);//central difference
     }   
 
-    public static Function1D integral(Function1D f){
-        return null;
+    public static double defIntegral(Function1D f, double lbound){
+
+        return x -> {
+             return 0;
+        };
     }
+    
+    public static Function1D antiDer(Function1D f){
+        return null;
+    } 
+
+
 }
