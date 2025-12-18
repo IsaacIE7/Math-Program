10904977b8b26c4414233c336b25e3783bc4a8a1
IsaacIE7 <isaacelgazali@gmail.com>
2025-12-17 21:08:26 -0500

    Fixed red squiggled line errors with classes.

--- MathProgram/src/Add.java
+++ MathProgram/src/Add.java
@@ -1,20 +1,23 @@
 public class Add implements Expression {
-    private Expression left, right;
+    private final Expression left, right;
 
     public Add(Expression left, Expression right){
         this.left = left;
         this.right = right;
     }
 
+    @Override
     public double evaluate(double x){
         return left.evaluate(x) + right.evaluate(x);
     }
 
+    @Override
     public Add sDerivative(){// returns new add because there are still a left and right being added(left prime and right prime)
         return new Add(left.sDerivative(), right.sDerivative());//A method that returns Add can override a method that returns Expr
                                                                 //This is called covariant return types.
     }
 
+    @Override
     public Expression simplify(){
         Expression sLeft = left.simplify(), sRight = right.simplify();
         if (sLeft instanceof Constant && ((Constant)sLeft).getValue() == 0.0){//needed to cast sLeft to consant and use getValue to compare to a double
@@ -31,6 +34,7 @@ public class Add implements Expression {
         return new Add(sLeft, sRight);
     }
 
+    @Override
     public String toString(){
         // return ("(" + left.toString() + " + " +  right.toString() + ")");//uses expressions unique toString
                 return left.toString() + " + " +  right.toString();//uses expressions unique toString
